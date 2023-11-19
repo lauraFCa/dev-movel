4a5e60619c6283732d6f2b7fbadb5b23212d2d65
Laura F Ca <laurafariacastro@gmail.com>
2023-11-19 17:55:30 -0300

    clean repo

--- imc-firebase/src/database/firebase.js
+++ imc-firebase/src/database/firebase.js
@@ -2,7 +2,12 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-  //config
+  apiKey: "AIzaSyA0aJZfor80yZ6fkWxZlMnC_XrDI5HUBQA",
+  authDomain: "devmovem-final.firebaseapp.com",
+  projectId: "devmovem-final",
+  storageBucket: "devmovem-final.appspot.com",
+  messagingSenderId: "781338308898",
+  appId: "1:781338308898:web:4fdbdb8ca7a5d1f840757b"
 };
 
 if (!firebase.apps.length)
